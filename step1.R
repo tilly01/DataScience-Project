9ab1fbd0783819375d94586ea4781f4c30e7755d
sophia <67592012+tilly01@users.noreply.github.com>
2023-02-22 15:30:08 +0100

    Update step1.R

--- step1.R
+++ step1.R
@@ -1,8 +1,8 @@
 library("dplyr")
 library("tidyr")
 
-## creates a list of one column
-target_list <- as.list(training_data$V123)
+##### creates a list of one column
+#target_list <- as.list(training_data$V123)
 
 ## creates a Class of words in a column (good way to explore how many different features there are)
 targets <- unique(training_data$V123)
@@ -18,34 +18,18 @@ data.frame(head(cds_mini))
 # view all cells for field 1
 complete_singlecell_data[complete_singlecell_data$V4==C10 & complete_singlecell_data$V2==P1 & complete_singlecell_data$V3==1]
 
-###### das auswählen einzelner Spalten
+###### das auswÃ¤hlen einzelner Spalten
 #csd_small <- data.frame(complete_singlecell_data %>% select(-(V6:V121)))
 #training_translation <- data.frame(training_data %>% select(-(V7:V122)))
 #VALI_SMALL <- data.frame(validation_data$V2)
 #plot(complete_singlecell_data$V8 > 3)
-left_join(complete_singlecell_data, training_data, by ("V121","V118"), keep=FALSE)
+
 
 #histogramm plotten
 hist(complete_singlecell_data$V8, freq=F, breaks = 20)
 
-
-
-
-
-############################################################################################
-## trying annotation and & filtering
-arrayfortry <- function (complete_singlecell_data, nWells=40, nPlates=4, nRep=2){
-  if (nWells*nPlates*nRep == nrow(complete_singlecell_data))
-      stop("Wrong dimension!")
-
-replicate <- rep(c(rep(1, nWells), rep(2, nWells)),
-                 nrow(complete_singlecell_data)/(nWells*nRep))
-complete_singlecell_data = complete_singlecell_data[order(replicate),]
-
-D=as.matrix(complete_singlecell_data)
-}
-############################################################################################
-
+# Spalten zusammenfügen um Loesungstabelle übersichtlich zu machen
+final_solution <- left_join(ground_truth_real, validation_decoding, by = "cell_code")
 
 ##### functions in dplyr
 #select, filter, arrange, mutate, summarise, group_by
@@ -61,4 +45,13 @@ AMPA_training <- training_data [training_data$V123 == "AMPA", ]
 Tubulin_training <- training_data [training_data$V123 == "Tubulin", ]
 
 
-#
+# bringt eine Tabelle mit nur Verbindung mit Ziel Targets hervor
+compund_targets <- subset(compund_annotations, V9 %in% c("Adrenoceptor", "AMPA", "Tubulin", "ROCK", "DNA_intercalation", "Topoll", "cMyc", "MEK", "dopaminereceptor", "CDK", "EGFR", "Ca2", "DNAMetabolism", "CellCycle", "rac1", "Cdc25", "eNOS")|
+                            V10 %in% c("Adrenoceptor", "AMPA", "Tubulin", "ROCK", "DNA_intercalation", "Topoll", "cMyc", "MEK", "dopaminereceptor", "CDK", "EGFR", "Ca2", "DNAMetabolism", "CellCycle", "rac1", "Cdc25", "eNOS") |
+                            V11 %in% c("Adrenoceptor", "AMPA", "Tubulin", "ROCK", "DNA_intercalation", "Topoll", "cMyc", "MEK", "dopaminereceptor", "CDK", "EGFR", "Ca2", "DNAMetabolism", "CellCycle", "rac1", "Cdc25", "eNOS") |
+                            V12 %in% c("Adrenoceptor", "AMPA", "Tubulin", "ROCK", "DNA_intercalation", "Topoll", "cMyc", "MEK", "dopaminereceptor", "CDK", "EGFR", "Ca2", "DNAMetabolism", "CellCycle", "rac1", "Cdc25", "eNOS") |
+                            V13 %in% c("Adrenoceptor", "AMPA", "Tubulin", "ROCK", "DNA_intercalation", "Topoll", "cMyc", "MEK", "dopaminereceptor", "CDK", "EGFR", "Ca2", "DNAMetabolism", "CellCycle", "rac1", "Cdc25", "eNOS") |
+                            V14 %in% c("Adrenoceptor", "AMPA", "Tubulin", "ROCK", "DNA_intercalation", "Topoll", "cMyc", "MEK", "dopaminereceptor", "CDK", "EGFR", "Ca2", "DNAMetabolism", "CellCycle", "rac1", "Cdc25", "eNOS") |
+                            V15 %in% c("Adrenoceptor", "AMPA", "Tubulin", "ROCK", "DNA_intercalation", "Topoll", "cMyc", "MEK", "dopaminereceptor", "CDK", "EGFR", "Ca2", "DNAMetabolism", "CellCycle", "rac1", "Cdc25", "eNOS"))
+
+
