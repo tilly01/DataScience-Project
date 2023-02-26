25a279e92b2dd9421ceb0dc5840a2ccb628320ab
sophia <67592012+tilly01@users.noreply.github.com>
2023-02-26 01:13:41 +0100

    Add files via upload

---
+++ 1_validation_decoding.R
@@ -0,0 +1,23 @@
+#validation_decoding
+
+library(dplyr)
+
+# creating a smaller sub_datatset for the big three
+csd <- data.frame(complete_singlecell_data %>% select(-(actin.s.area:dist.30.nn)))
+valid<- data.frame(validation_data %>% select(-(actin.s.area:dist.30.nn)))
+valid<- data.frame(valid %>% select(-(well_code)))
+training<- data.frame(training_data %>% select(-(actin.s.area:dist.30.nn)))
+training<- data.frame(training %>% select(-(target)))
+training<- data.frame(training %>% select(-(cell_id:field)))
+
+# validation + training
+merge_valid_training <- merge(training, valid, by = c("nuclear.displacement", "cell_code"), all = TRUE)
+result <- merge_valid_training[merge_valid_training$cell_code %in% valid$cell_code, ]
+
+# backup with data from complete_singlecell_data
+finalmerge <- merge (merge_valid_training, csd, by=c("nuclear.displacement"), all=TRUE)
+finalmerge <- data.frame(finalmerge %>% select(-(nuclear.displacement)))
+
+# subtract the training data for final decoding
+result <- finalmerge[finalmerge$cell_code %in% valid$cell_code, ]
+
---
+++ 2_target_definition.R
@@ -0,0 +1,22 @@
+#for target definition
+
+truth <- data.frame(result)
+
+truth$target <- ifelse(truth$well %in% c("A15", "B12", "M14"), "CDK",
+                  ifelse(truth$well %in% c("B20", "B11", "B21", "C18"), "dopaminereceptor",
+                  ifelse(truth$well %in% c("F15", "B15", "F19", "P13"), "EGFR",
+                  ifelse(truth$well %in% c("D15", "C10", "F18"), "adrenoceptor",
+                  ifelse(truth$well %in% c("K10", "C22", "O10"), "Topoll",
+                  ifelse(truth$well %in% c("P14", "H22", "D20"), "DNAMetabolism",
+                  ifelse(truth$well %in% c("G12", "E11"), "AMPA",                         
+                  ifelse(truth$well %in% c("H21", "F17", "L14"), "DNA_Intercalation",
+                  ifelse(truth$well %in% c("I23", "G23", "N22"), "Tubulin",
+                  ifelse(truth$well %in% c("M11", "H18"), "CellCycle",
+                  ifelse(truth$well %in% c("M23", "K23"), "MEK",
+                  ifelse(truth$well == "I15", "eNOS",
+                  ifelse(truth$well == "I11", "rac1",                          
+                  ifelse(truth$well == "N20", "Cdc25",                         
+                  ifelse(truth$well == "K20", "AMPA/Ca2",
+                  ifelse(truth$well == "E23", "ROCK",
+                  ifelse(truth$well == "P16", "Ca2", 
+                  ifelse(truth$well =="I13", "cMyc", "  "))))))))))))))))))
