2c68763030d74de397d1be433b4f3496fdd9239f
sophia <67592012+tilly01@users.noreply.github.com>
2023-02-26 22:08:45 +0100

    1_validation_decoding
    step one of the pipeline, including a data prep for the deletion of empty cells

--- 1_validation_decoding.R
+++ 1_validation_decoding.R
@@ -1,6 +1,23 @@
-#validation_decoding
-
 library(dplyr)
+
+### DATA PREP
+
+# Identify rows with empty cells
+empty_csd <- which(apply(is.na(complete_singlecell_data), 1, all))
+empty_train <- which(apply(is.na(training_data), 1, all))
+empty_valid <- which(apply(is.na(validation_data), 1, all))
+empty_compound <- which(apply(is.na(compund_annotations), 1, all))
+empty_WT <- which(apply(is.na(annotations_WT_H1047_MarcoBreinig), 1, all))
+
+# Subset the dataset to exclude empty rows
+complete_singlecell_data <- complete_singlecell_data[complete.cases(complete_singlecell_data), ]
+training_data <- training_data[complete.cases(training_data), ]
+validation_data <- validation_data[complete.cases(validation_data), ]
+compund_annotations <- compund_annotations[complete.cases(compund_annotations), ]
+annotations_WT_H1047_MarcoBreinig <- annotations_WT_H1047_MarcoBreinig[complete.cases(annotations_WT_H1047_MarcoBreinig), ]
+
+
+### VALIDATION DECODING
 
 # creating a smaller sub_datatset for the big three
 csd <- data.frame(complete_singlecell_data %>% select(-(actin.s.area:dist.30.nn)))
